751f3ea2169f49a14ab682520f1556b4304dd7fc
Andrea Lee <andrealee053@gmail.com>
2019-12-14 17:42:07 -0500

    implemented update button for valid width and height values

--- src/components/edit_screen/EditScreen.js
+++ src/components/edit_screen/EditScreen.js
@@ -7,6 +7,7 @@ import Modal from 'react-materialize/lib/Modal';
 import { getFirestore } from 'redux-firestore';
 
 import WireframeDisplay from './WireframeDisplay'
+import Button from 'react-materialize/lib/Button';
 
 class EditScreen extends Component {
     state = {
@@ -17,6 +18,7 @@ class EditScreen extends Component {
         original: this.props.wireframe.controls,
         currentWork: this.props.wireframe.controls,
         showModal: false,
+        disable: true,
     }
 
     handleChange = (e) => {
@@ -31,6 +33,26 @@ class EditScreen extends Component {
         this.props.firestore.collection("wireframes").doc(this.props.wireframe.id).update( {
             [target.id] : target.value
         });
+    }
+
+    updateChange = (e) => {
+        const { target } = e;
+        
+
+        if (target.id == "completed") {
+            this.setState(state => ({
+                ...state,
+                [target.id]: target.checked,
+            }));
+        } else {
+            this.setState(state => ({
+                ...state,
+                [target.id]: target.value,
+            }),function(){
+                this.disableUpdateButton();
+            });
+        }
+
     }
 
     showModal() {
@@ -54,6 +76,13 @@ class EditScreen extends Component {
 
     handleSave = () => {
         console.log("Diagram Saved.");
+
+
+
+
+        //set original
+
+
     }
 
     handleConfirm = () => {
@@ -67,10 +96,28 @@ class EditScreen extends Component {
     }
 
     updateDimensions = () => {
-        // Non-integer dimension or integers smaller than 1 or larger than 5000 should be disregarded and should not update the diagram. 
-        
-        
+        this.setState({
+            disable: true
+        },function(){
+            // update database
+            this.props.firestore.collection("wireframes").doc(this.props.wireframe.id).update( {
+                width : this.state.width,
+                height : this.state.height
+            });
+        });
+    }
 
+    disableUpdateButton(){
+        // Non-integer dimension or integers smaller than 1 or larger than 5000 should be disregarded and should not update the diagram.
+        let bool = false;
+        if(this.state.width<1 | this.state.height<1 | this.state.width>5000 | this.state.height>5000 
+            | (this.state.width==this.props.wireframe.width && this.state.height==this.props.wireframe.height) ) {
+            bool = true;
+        }
+        
+        this.setState({
+            disable: bool
+        });
     }
 
     render() {
@@ -94,14 +141,24 @@ class EditScreen extends Component {
                         <div className="row">
                             <div className="col s3 input-field">
                                 <label htmlFor="email" className="active">Width</label>
-                                <input type="text" name="width" id="width" onChange={this.handleChange} value={wireframe.width} />
+                                <input type="number" name="width" id="width" onChange={this.updateChange} value={this.state.width} />
                             </div>
                             
                             <i className="col s1 cross_icon material-icons">clear</i>
+                            
                             <div className="col s3 input-field">
                                 <label htmlFor="email" className="active">Height</label>
-                                <input type="text" name="height" id="height" onChange={this.handleChange} value={wireframe.height} />
+                                <input type="number" name="height" id="height" onChange={this.updateChange} value={this.state.height} />
                             </div>
+
+                            <div className="col s3">
+                                <Button
+                                    style={{marginLeft: "25px", marginTop: "25px"}}
+                                    disabled={this.state.disable}
+                                    onClick={this.updateDimensions}>
+                                Update</Button>
+                            </div>
+                            
                         </div>
                         
 
@@ -113,7 +170,7 @@ class EditScreen extends Component {
 
                     <div className="col s6">
                         <div className="wireframe_container">
-                            <div className="diagram" style={{width: this.state.width, height: this.state.height}}>
+                            <div className="diagram" style={{width: wireframe.width, height: wireframe.height}}>
                                 <WireframeDisplay wireframe={wireframe} />
                             </div>
                         </div>
